874d8fb90355ee345f9e917e902be2e8a8108aca
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-09-12 16:32:32 +0100

    Instantiated ObjectMapper and created testSerialization

--- src/test/java/org/example/PersonDTOTest.java
+++ src/test/java/org/example/PersonDTOTest.java
@@ -1,5 +1,27 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class PersonDTOTest {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void testSerialization() throws JsonProcessingException {
+
+        PersonDTO person = new PersonDTO();
+        person.setName("John Doe");
+        person.setAge(30);
+        person.setEmail("john.doe@test.com");
+
+        String result = objectMapper.writeValueAsString(person);
+
+        Assertions.assertEquals("{" + "\"name\":\"John Doe\"," + "\"age\":30," +
+                "\"email\":\"john.doe@test.com\"" + "}", result);
+
+    }
 
 }
