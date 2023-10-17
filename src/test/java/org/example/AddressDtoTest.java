646e16dad41660e8fa67feb16445e93916b23519
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-10-17 14:59:07 +0100

    Updated testSerialization method in AddressDtoTest to utilise JSONAssert, testDeserialization to test the fields, renamed variables to conform to practice norms and included comment annotation

--- src/test/java/org/example/AddressDtoTest.java
+++ src/test/java/org/example/AddressDtoTest.java
@@ -2,16 +2,20 @@ package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 class AddressDtoTest {
 
-    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectMapper underTest = new ObjectMapper();
 
     @Test
-    void testSerialization() throws JsonProcessingException {
+    void testSerialization() throws JsonProcessingException, JSONException {
 
+        //given
         AddressDto address = new AddressDto();
         address.setAddressLine1("123");
         address.setAddressLine2("Example Road");
@@ -19,16 +23,19 @@ class AddressDtoTest {
         address.setCouncil("Example Council");
         address.setPostcode("123 4567");
 
-        String result = objectMapper.writeValueAsString(address);
+        //when
+        String actual = underTest.writeValueAsString(address);
 
-        Assertions.assertEquals("{\"addressLine1\":\"123\",\"addressLine2\":\"Example Road\",\"city\":\"Example City\"," +
-                "\"council\":\"Example Council\",\"postcode\":\"123 4567\"}", result);
+        //then
+        JSONAssert.assertEquals("{\"addressLine1\":\"123\",\"addressLine2\":\"Example Road\",\"city\":\"Example City\"," +
+                "\"council\":\"Example Council\",\"postcode\":\"123 4567\"}", actual, JSONCompareMode.LENIENT);
 
     }
 
     @Test
     void testDeserialization() throws JsonProcessingException {
 
+        //given
         AddressDto address = new AddressDto();
         address.setAddressLine1("123");
         address.setAddressLine2("Example Road");
@@ -36,11 +43,17 @@ class AddressDtoTest {
         address.setCouncil("Example Council");
         address.setPostcode("123 4567");
 
-        AddressDto result = objectMapper.readValue(
+        //when
+        AddressDto actual = underTest.readValue(
                 "{\"addressLine1\":\"123\",\"addressLine2\":\"Example Road\",\"city\":\"Example City\"," +
                         "\"council\":\"Example Council\",\"postcode\":\"123 4567\"}", AddressDto.class);
 
-        Assertions.assertEquals(address.getClass(), result.getClass());
+        //then
+        Assertions.assertEquals(address.getAddressLine1(), actual.getAddressLine1());
+        Assertions.assertEquals(address.getAddressLine2(), actual.getAddressLine2());
+        Assertions.assertEquals(address.getCity(), actual.getCity());
+        Assertions.assertEquals(address.getCouncil(), actual.getCouncil());
+        Assertions.assertEquals(address.getPostcode(), actual.getPostcode());
 
     }
 
